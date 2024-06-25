970edafeeabe63b5e60fa6811ad254806f1c9a27
home_jay <home_jay@>
2024-06-26 03:39:22 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,19 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+  // plugins: [
+  //   vue(),
+  // ],
+  // resolve: {
+  //   alias: {
+  //     '@': fileURLToPath(new URL('./src', import.meta.url))
+  //   }
+  // }
+  
+  server: {
+    // 指定開發服務器的主機名（預設是 'localhost'）
+    host: '0.0.0.0',
+    port: 80,
+  },
+
 })
